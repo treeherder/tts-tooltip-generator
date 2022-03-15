acadeecab3791dbc3c23407d3b780e326ffef087
brendan <brendan@sproutfi.com>
2022-03-14 19:46:32 -0500

    fix gitignore effect

--- tooltips/target/debug/deps/tooltips-790cdef1fe622954.d
+++
@@ -1,7 +0,0 @@
-/home/fiver/projects/simple-scribe/tooltips/target/debug/deps/tooltips-790cdef1fe622954.rmeta: src/main.rs
-
-/home/fiver/projects/simple-scribe/tooltips/target/debug/deps/tooltips-790cdef1fe622954.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
